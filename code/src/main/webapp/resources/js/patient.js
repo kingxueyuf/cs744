d05da4dc164512ee7a35ee853b0f89591744c611
sanwazi <chu.jing@uwlax.edu>
2015-02-26 14:56:32 -0600

    updated href

--- code/src/main/webapp/resources/js/patient.js
+++ code/src/main/webapp/resources/js/patient.js
@@ -117,7 +117,7 @@ function updateButton() {
 					$button.attr("href", "emr.html?patient_id=" + patient_id);
 				} else if (data['hasEmr'] == "false") {
 					$button.attr('class', 'btn btn-danger btn-xs');
-					$button.attr('herf', 'create_emr.html?id=' + patient_id);
+					$button.attr('href', 'create_emr.html?id=' + patient_id);
 					
 					
 				}
